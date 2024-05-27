75b542a6c27e0749141e16b250b9d98a5804e9ac
AdishtJaglan <adisht7524@gmail.com>
2024-05-27 16:48:40 +0530

    create class that creates and instanties a board

---
+++ src/board.js
@@ -0,0 +1,25 @@
+export class Board {
+  constructor() {
+    this.rows = 10;
+    this.columns = 10;
+    this.board = [];
+
+    for (let i = 0; i < this.rows; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        this.board.push(Cell());
+      }
+    }
+  }
+
+  getBoard() {
+    return this.board;
+  }
+}
+
+const Cell = () => {
+  let isEmpty = true;
+  let shipName;
+
+  return { isEmpty, shipName };
+};
